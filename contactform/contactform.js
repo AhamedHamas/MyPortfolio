8fc8157120c22ee3da84780f52f3414767eb5cd9
AhamedHamas <hamasafa.21@uom.lk>
2022-10-10 21:35:52 +0530

    updated Final

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,38 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+let Name = document.getElementById("name").value;
+let Email = document.getElementById("email").value;
+let Subject = document.getElementById("subject").value;
+let Message = document.getElementById("message").value;
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAKfBLo-Rjsy5T9Yn_sKftRmLeUTxyoUGk",
-  authDomain: "hellofirebase-b9e7f.firebaseapp.com",
-  databaseURL: "https://hellofirebase-b9e7f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hellofirebase-b9e7f",
-  storageBucket: "hellofirebase-b9e7f.appspot.com",
-  messagingSenderId: "300587045356",
-  appId: "1:300587045356:web:a63a33b446db4c1b42f0c9"
-};
+function validateForm(){
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  if (4 > Name || Name > 20){
+    alert("Enter a name between 4 to 20 characters");
+    return;
+  }
 
-const db =  getDatabase(app);
+  let emailRegEx=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(!emailRegEx.test(Email))
+  {
+  alert("Invalid Email");
+  return;
+  }
 
-document.getElementById("contactForm").addEventListener("submit",function(onclick){
-    onclick.preventDefault();
-    let name = document.getElementById("name").value;
-    let Email = document.getElementById("email").value;
-    let Subject = document.getElementById("subject").value;
-    let Message = document.getElementById("message").value;
-    
+  if (Subject == "")
+  {
+    alert ("Please enter a subject");
+    return;
+  }
 
-    set(ref(db),{
-        username: name,
-        Email: Email,
-        Subject: Subject,
-        Message: Message,
-    });
 
-})
+
+}
